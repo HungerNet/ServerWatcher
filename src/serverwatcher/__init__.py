8095923d60013c63f4492e5b2f6248113fbd74e7
iFamishedX <me@ifamished.com>
2026-04-22 20:51:03 +0000

    Update init

--- src/serverwatcher/__init__.py
+++ src/serverwatcher/__init__.py
@@ -2,32 +2,18 @@ from importlib.metadata import version as _pkg_version, PackageNotFoundError
 
 # Package version
 try:
-    __version__ = _pkg_version("hungerlib")
+    __version__ = _pkg_version('serverwatcher')
 except PackageNotFoundError:
-    __version__ = "0.0.0"
+    __version__ = '0.0.0'
 
-# --- Core modules ---
-from .panel import Panel
-from .logger import HungerLogger
+from .watcher import ServerWatcher
+from .config import WatcherConfig
+from .messages import WatcherMessages
 
-# --- API endpoints ---
-from .api.schedule import ScheduleAPI
-from .api.filemanager import FileManagerAPI
-from .api.backups import BackupsAPI
-from .api.databases import DatabasesAPI
-from .api.startup import StartupAPI
 
 __all__ = [
-    "__version__",
-
     # core utilities
-    "Panel",
-    "HungerLogger",
-
-    # API endpoints
-    "ScheduleAPI",
-    "FileManagerAPI",
-    "BackupsAPI",
-    "DatabasesAPI",
-    "StartupAPI",
+    'ServerWatcher',
+    'WatcherConfig,
+    'WatcherMessages',
 ]
