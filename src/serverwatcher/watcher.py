14c880ab623318b22b339c70c330d39301bdc102
iFamishedX <me@ifamished.com>
2026-04-24 21:18:33 +0000

    Fixed imports

--- src/serverwatcher/watcher.py
+++ src/serverwatcher/watcher.py
@@ -16,9 +16,9 @@ from hungerlib.addons import (
 )
 
 # NEW CONFIG DATACLASSES
-from config.global_config import GlobalConfig
-from config.messages import MessagesConfig
-from config.watcher import WatcherConfig
+from serverwatcher.config.global_config import GlobalConfig
+from serverwatcher.config.messages import MessagesConfig
+from serverwatcher.config.watcher import WatcherConfig
 
 
 # ---------------------------------------------------------
