a2a1682b7ef8030cf2dd0ed2c9134b87a149b75f
iFamishedX <me@ifamished.com>
2026-04-27 17:47:30 +0000

    Removed broken schema import

--- src/serverwatcher/__init__.py
+++ src/serverwatcher/__init__.py
@@ -7,13 +7,9 @@ except PackageNotFoundError:
     __version__ = '0.0.0'
 
 from .watcher import ServerWatcher
-from .schema import validate_config_schema, validate_messages_schema, flatten_nested
 
 
 __all__ = [
     # core utilities
     'ServerWatcher',
-    'validate_config_schema',
-    'validate_messages_schema',
-    'flatten_nested',
 ]
