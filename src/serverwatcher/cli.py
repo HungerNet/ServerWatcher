f06988c1e176475dcd78f9e500a105eb06c4b510
iFamishedX <me@ifamished.com>
2026-08-11 16:49:37 -0500

    debug

--- src/serverwatcher/cli.py
+++ src/serverwatcher/cli.py
@@ -63,7 +63,7 @@ class WatcherCLI(cmd.Cmd):
         view watcher
         """
         if arg == "cli":
-            bprint("test")
+            self.bprint("test")
             self._view_cli()
         elif arg == "watcher":
             self._view_watcher()
