04ce237ec74980d8142f80f75290bc5701733082
TurtleMaster10 <turtlemaster10@hungernet.dev>
2026-09-21 16:56:50 -0500

    Fixed `validateAll()` usage

--- src/serverwatcher/targetserver.py
+++ src/serverwatcher/targetserver.py
@@ -61,7 +61,7 @@ class TargetServer:
 
     def checkPtero(self):
         '''Check if the panel is reachable and the API key is valid.'''
-        return utils.validateAll()
+        return utils.validateAll(panel=self.panel, server=self.server)
 
     def checkBridge(self):
         '''Check if the bridge is reachable and the token is valid.'''
