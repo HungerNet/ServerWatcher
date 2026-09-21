b3195999f1842849699c880a0ba1c8e5fa4f91d1
TurtleMaster10 <turtlemaster10@hungernet.dev>
2026-09-21 12:15:50 -0500

    Fix m.bind issue

--- src/serverwatcher/targetserver.py
+++ src/serverwatcher/targetserver.py
@@ -34,28 +34,28 @@ class TargetServer:
         self.bridge_restart_handler = bridge_restart_handler
         self.bridge_kill_handler = bridge_kill_handler
 
-        m.proxy(self.bridge.getTokenInfo)
+        m.bind(self, self.bridge.getTokenInfo)
 
-        m.proxy(self.bridge.getPlatform)
-        m.proxy(self.bridge.getMinecraftVersion)
-        m.proxy(self.bridge.getBridgeVersion)
-        m.proxy(self.bridge.getBridgePort)
+        m.bind(self, self.bridge.getPlatform)
+        m.bind(self, self.bridge.getMinecraftVersion)
+        m.bind(self, self.bridge.getBridgeVersion)
+        m.bind(self, self.bridge.getBridgePort)
 
-        m.proxy(self.bridge.getPlayers)
-        m.proxy(self.bridge.getMaxPlayers)
+        m.bind(self, self.bridge.getPlayers)
+        m.bind(self, self.bridge.getMaxPlayers)
 
-        m.proxy(self.bridge.getTPS)
-        m.proxy(self.bridge.getMSPT)
+        m.bind(self, self.bridge.getTPS)
+        m.bind(self, self.bridge.getMSPT)
 
-        m.proxy(self.bridge.getLoadedChunks)
-        m.proxy(self.bridge.getLoadedEntities)
+        m.bind(self, self.bridge.getLoadedChunks)
+        m.bind(self, self.bridge.getLoadedEntities)
 
-        m.proxy(self.bridge.getGCStats)
-        m.proxy(self.bridge.getThreadStats)
-        m.proxy(self.bridge.getNetworkStats)
+        m.bind(self, self.bridge.getGCStats)
+        m.bind(self, self.bridge.getThreadStats)
+        m.bind(self, self.bridge.getNetworkStats)
 
-        m.proxy(self.bridge.log)
-        m.proxy(self.bridge.runCommand)
+        m.bind(self, self.bridge.log)
+        m.bind(self, self.bridge.runCommand)
 
         self.stream = self.bridge.stream
 
@@ -78,9 +78,10 @@ class TargetServer:
     def refreshResources(self):
         '''Refreshes the server's resources (ptero only)'''
         if self.ptero_enabled:
-            self.mc_server.refresh()
+            return self.mc_server.refresh()
         else:
-            raise UnsupportedOperationError('Bridge-only mode: refreshing is not necessary and not supported')
+            return
+            # raise UnsupportedOperationError('Bridge-only mode: refreshing is not necessary and not supported')
 
     def getRAM(self):
         '''Returns current RAM usage'''
@@ -94,7 +95,8 @@ class TargetServer:
         if self.ptero_enabled:
             return self.mc_server.getDisk(rounding=0, gb=False)
         else:
-            raise UnsupportedOperationError('Bridge-only mode: getting disk usage is not supported')
+            return
+            # raise UnsupportedOperationError('Bridge-only mode: getting disk usage is not supported')
 
     def getCPULoad(self):
         '''Returns current CPU usage'''
@@ -121,7 +123,8 @@ class TargetServer:
         elif self.bridge_start_handler is not None:
             self.bridge_start_handler()
         else:
-            raise UnsupportedOperationError('Bridge-only mode: starting the server is not supported unless a bridge start handler is provided.')
+            return
+            # raise UnsupportedOperationError('Bridge-only mode: starting the server is not supported unless a bridge start handler is provided.')
     
     def restartServer(self):
         '''Restarts the server (ptero-only)'''
@@ -130,7 +133,8 @@ class TargetServer:
         elif self.bridge_restart_handler is not None:
             self.bridge_restart_handler()
         else:
-            raise UnsupportedOperationError('Bridge-only mode: restarting the server is not supported unless a bridge restart handler is provided.')
+            return
+            # raise UnsupportedOperationError('Bridge-only mode: restarting the server is not supported unless a bridge restart handler is provided.')
 
     def stopServer(self):
         '''Stops the server (ptero and bridge)'''
@@ -146,4 +150,5 @@ class TargetServer:
         elif self.bridge_kill_handler is not None:
             self.bridge_kill_handler()
         else:
-            raise UnsupportedOperationError('Bridge-only mode: killing the server is not supported unless a bridge kill handler is provided.')
+            return
+            # raise UnsupportedOperationError('Bridge-only mode: killing the server is not supported unless a bridge kill handler is provided.')
